c93a92f6b23c960cc38e2cfe8f34e05dd94b2321
Temur5319436 <temur5319436@gmail.com>
2023-07-26 10:14:34 +0500

    fix: i have changed error response

--- src/Exceptions/NoAbilityException.php
+++ src/Exceptions/NoAbilityException.php
@@ -3,7 +3,12 @@
 namespace Usmonaliyev\LaravelRedisAuth\Exceptions;
 
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
 
 class NoAbilityException extends HttpResponseException
 {
+  public function __construct(string $message, int $status = Response::HTTP_FORBIDDEN)
+  {
+    parent::__construct(response()->json(['message' => $message], $status));
+  }
 }
